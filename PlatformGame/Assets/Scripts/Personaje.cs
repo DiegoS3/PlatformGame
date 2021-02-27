38784fd5a6bed575e399c48a6cc445a2cc539852
Guillermo <guillerapc@gmail.com>
2021-02-27 22:55:18 +0100

    Pj working (Script + checkGround + Animation)

--- PlatformGame/Assets/Scripts/Personaje.cs
+++ PlatformGame/Assets/Scripts/Personaje.cs
@@ -5,112 +5,158 @@ using UnityEngine;
 public class Personaje : MonoBehaviour
 {
 
-    private bool saltando;
-    private bool atacando;
-    private int vidas;
-    private int monedas;
+    public float runSpeed = 2;
+    public float jumpSpeed = 3;
 
-    public float salto;
-    public GameObject atqOG;
-    public GameObject atqPosition;
+    public bool betterJump = false;
+    public float fallMultiplier = 0.5f;
+    public float lowJumpMultiplier = 1f;
+
+    Rigidbody2D rb2D;
+    public SpriteRenderer spriteRenderer;
+    public Animator animator;
+
+    public float walljumpforce;
+    public Vector2 walljumpAngle;
+
+    bool isTouchingFront = false;
+    bool wallSliding;
+    public float wallSlidingSpeed = 0.75f;
+    bool isTouchingRight;
+    bool isTouchingLeft;
+
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-        saltando = false;
-        atacando = false;
-        vidas = 3;
-        monedas = 0;
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+
+        if (isTouchingFront && !CheckGround.isGrounded)
+        {
+            wallSliding = true;
+        }
+        else
+        {
+            wallSliding = false;
+        }
+
+        if (wallSliding)
+        {
+            animator.Play("PjSlide");
+            rb2D.velocity = new Vector2(rb2D.velocity.x, Mathf.Clamp(rb2D.velocity.y, -wallSlidingSpeed, float.MaxValue));
+
+        }
+        if (!CheckGround.isGrounded)
+        {
+            animator.SetBool("Jump", true);
+            animator.SetBool("Run", false);
+            //animator.Play("PjFall");
+
+        }
+
+        if (CheckGround.isGrounded)
+        {
+            animator.SetBool("Jump", false);
+        }
+
+
+
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey("d") || Input.GetKey("right") && !isTouchingRight)
         {
-            transform.Translate(new Vector3(-0.1f, 0.0f));
+            rb2D.velocity = new Vector2(runSpeed, rb2D.velocity.y);
+            spriteRenderer.flipX = false;
+            animator.SetBool("Run", true);
+        }
+        else if (Input.GetKey("a") || Input.GetKey("left") && !isTouchingLeft)
+        {
+            rb2D.velocity = new Vector2(-runSpeed, rb2D.velocity.y);
+            spriteRenderer.flipX = true;
+            animator.SetBool("Run", true);
         }
         else
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            transform.Translate(new Vector3(0.1f, 0.0f));
+            rb2D.velocity = new Vector2(0, rb2D.velocity.y);
+            animator.SetBool("Run", false);
+        }
+        if (Input.GetKey("w") && CheckGround.isGrounded && !wallSliding)
+        {
+            
+            rb2D.velocity = new Vector2(rb2D.velocity.x, jumpSpeed);
+            
+            
         }
 
-        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        if (Input.GetKey("w") && wallSliding)
         {
-            if (!saltando)
+            rb2D.velocity = new Vector3(-rb2D.velocity.x, jumpSpeed, rb2D.velocity.y);
+        }
+
+       
+
+        if (betterJump && !wallSliding)
+        {
+            if (rb2D.velocity.y < 0)
             {
-                GetComponent<Rigidbody2D>().AddForce(new Vector2(0.0f, salto));
-                saltando = true;
-            }            
-        }
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            if (!atacando)
-            {
-                GameObject.Instantiate(atqOG, atqPosition.transform.position, atqPosition.transform.rotation);
-                atacando = true;
-                Invoke("TerminarAtaque", 0.5f);
-            }            
-        }
-
-    }
-
-    private void TerminarAtaque()
-    {
-        atacando = false;
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag.Equals("Suelo"))
-        {
-            saltando = false;
-        }
-
-        if (collision.gameObject.tag.Equals("Enemigo"))
-        {           
-            collision.gameObject.SetActive(false);
-            Destroy(collision.gameObject, 0.5f);
-            vidas--;
-            Debug.Log("Vidas " + vidas);
-
-            if (vidas <= 0)
-            {
-                Debug.Log("El jugador ha muerto");
-                gameObject.SetActive(false);
-
-                int recorUltimo = PlayerPrefs.GetInt("Monedas");
-                if (!PlayerPrefs.HasKey("Monedas"))
-                {
-                    //No hay record guardado
-                    PlayerPrefs.SetInt("Monedas", monedas);
-                    //TODO: poner aqui una pantalla de nuevo record
-                }
-                else
-                {
-                    //Si hay record guardado
-                    if (recorUltimo < monedas)
-                    {
-                        //Nuevo record
-                        PlayerPrefs.SetInt("Monedas", monedas);
-                    }
-                }
+                rb2D.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier) * Time.deltaTime;
             }
+            if (rb2D.velocity.y > 0 && !Input.GetKey("w"))
+            {
+                rb2D.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier) * Time.deltaTime;
+            }
+        }
 
+        if (Input.GetKey("space"))
+        {
+            animator.SetBool("Attack", true);
+            animator.SetBool("Run", false);
+        }
+        else
+        {
+            animator.SetBool("Attack", false);
+        }
+
+        if (isTouchingFront && !CheckGround.isGrounded)
+        {
+            animator.SetBool("Falling", true);
+        }
+        else
+        {
+            animator.SetBool("Falling", false);
+        }
+
+    }
+
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("RigthWall"))
+        {
+            isTouchingFront = true;
+            isTouchingRight = true;
+        }
+        if (collision.gameObject.CompareTag("LeftWall"))
+        {
+            isTouchingFront = true;
+            isTouchingLeft = true;
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnCollisionExit2D(Collision2D collision)
     {
-        if (collision.gameObject.tag.Equals("Moneda"))
-        {
-            collision.gameObject.SetActive(false);
-            Destroy(collision.gameObject, 0.5f);
-            monedas++;
-            Debug.Log("Monedas " + monedas);
-        }
+        isTouchingFront = false;
+        isTouchingLeft = false;
+        isTouchingRight = false;
     }
 
 }
