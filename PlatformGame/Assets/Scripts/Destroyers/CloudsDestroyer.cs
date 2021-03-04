cebeb440eaca14c40a0865e2c64f2f80a6fb710e
Diego <diegozj92@gmail.com>
2021-03-04 14:22:50 +0100

    Generator Clouds, CheckPoint Bug

---
+++ PlatformGame/Assets/Scripts/Destroyers/CloudsDestroyer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudsDestroyer : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Cloud"))
+        {
+            collision.gameObject.SetActive(false);
+            Destroy(collision.gameObject, 0.2f);
+        }
+    }
+}
---
+++ PlatformGame/Assets/Scripts/Props/Cloud.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloud : MonoBehaviour
+{
+    public GameObject cloud;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InvokeRepeating("MoveCloud", 0f, 0.02f);        
+    }
+
+    private void MoveCloud()
+    {
+        float random = Random.Range(0.0f, 0.002f);
+        Vector3 cloudPosition = cloud.transform.position;
+        cloudPosition.x -= random;
+        cloud.transform.position = cloudPosition;
+    }
+
+    public void StopMoveCloud()
+    {
+        CancelInvoke("MoveCloud");
+    }
+
+}
